60ea9cd662b58d1df5091fc47ceaa3c2b14a2d3c
Aasurjya <ahandique8@gmail.com>
2024-11-24 12:21:02 +0530

    Initial Commit

---
+++ PointcloudTests/PointcloudTests.swift
@@ -0,0 +1,17 @@
+//
+//  PointcloudTests.swift
+//  PointcloudTests
+//
+//  Created by Aasurjya Handique on 24/11/24.
+//
+
+import Testing
+@testable import Pointcloud
+
+struct PointcloudTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
